1e24ffb770adffdba5a91ab87a6d3215e424a9ee
ayush24-eth <ayushtmsl611@gmail.com>
2023-10-06 15:40:01 +0530

    optimsed and remove comments

--- contracts/crypto_exchange.sol
+++ contracts/crypto_exchange.sol
@@ -39,6 +39,7 @@ contract CryptoExchange {
     function deposit(address tokenAddress, uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
         require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), "Transfer failed");
+
         balances[msg.sender] += amount;
         emit Deposit(msg.sender, amount);
     }
@@ -46,8 +47,10 @@ contract CryptoExchange {
     function withdraw(address tokenAddress, uint256 amount) external {
         require(amount > 0, "Amount must be greater than 0");
         require(balances[msg.sender] >= amount, "Insufficient balance");
+
         balances[msg.sender] -= amount;
         require(IERC20(tokenAddress).transfer(msg.sender, amount), "Transfer failed");
+        
         emit Withdrawal(msg.sender, amount);
     }
 
@@ -76,176 +79,8 @@ contract CryptoExchange {
     }
     
     function getBalance(address user_account) external returns (uint){
-    
-       string memory data = "User Balance is : ";
        uint user_bal = user_account.balance;
-       emit CheckBalance(data, user_bal );
+       emit CheckBalance(user_bal);
        return (user_bal);
-
     }
 }
-
-/*********************************************************************************************************************************************/
-
-// 2. Smart contract for De-centralized Exchange
-
-/*
-contract CryptoExchange {
-    address public owner;
-
-    event TradeExecuted(
-        address indexed trader,
-        address indexed fromToken,
-        address indexed toToken,
-        uint256 fromAmount,
-        uint256 toAmount
-    );
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
-    }
-
-    function executeTrade(
-        address fromToken,
-        address toToken,
-        uint256 fromAmount
-    ) external {
-        require(fromToken != toToken, "Cannot trade the same token");
-        require(fromAmount > 0, "Amount must be greater than 0");
-
-        // In a real DEX, you would perform order matching, handle liquidity pools, and other complex logic.
-        // This example assumes a 1:1 token swap for simplicity.
-
-        require(IERC20(fromToken).transferFrom(msg.sender, address(this), fromAmount), "Transfer failed");
-        uint256 toAmount = fromAmount;
-        require(IERC20(toToken).transfer(msg.sender, toAmount), "Transfer failed");
-
-        emit TradeExecuted(msg.sender, fromToken, toToken, fromAmount, toAmount);
-    }
-
-    // Additional functions for managing liquidity pools, order books, and other DEX features can be added.
-}
-*/
-
-/*********************************************************************************************************************************************/
-
-// 3. Smart contract for Hybrid Exchange
-
-/*
-contract CryptoExchange {
-    address public owner;
-    mapping(address => uint256) public balances;
-    uint256 public tradeFee; // Fee in basis points (1/100th of a percentage)
-    uint256 public orderIdCounter;
-
-    struct Order {
-        address trader;
-        address fromToken;
-        address toToken;
-        uint256 fromAmount;
-        uint256 toAmount;
-        bool isBuyOrder;
-        bool isExecuted;
-    }
-
-    Order[] public orders;
-
-    event Deposit(address indexed user, uint256 amount);
-    event Withdrawal(address indexed user, uint256 amount);
-    event TradeExecuted(
-        address indexed user,
-        uint256 orderId,
-        address indexed fromToken,
-        address indexed toToken,
-        uint256 fromAmount,
-        uint256 toAmount,
-        uint256 fee
-    );
-
-    constructor(uint256 _tradeFee) {
-        owner = msg.sender;
-        tradeFee = _tradeFee;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
-    }
-
-    function deposit(address tokenAddress, uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount), "Transfer failed");
-        balances[msg.sender] += amount;
-        emit Deposit(msg.sender, amount);
-    }
-
-    function withdraw(address tokenAddress, uint256 amount) external {
-        require(amount > 0, "Amount must be greater than 0");
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        require(IERC20(tokenAddress).transfer(msg.sender, amount), "Transfer failed");
-        emit Withdrawal(msg.sender, amount);
-    }
-
-    function createOrder(
-        address fromToken,
-        address toToken,
-        uint256 fromAmount,
-        uint256 toAmount,
-        bool isBuyOrder
-    ) external {
-        require(fromToken != toToken, "Invalid token pair");
-        require(fromAmount > 0 && toAmount > 0, "Invalid order amount");
-        require(fromAmount * toAmount > 0, "Invalid order value");
-        require(balances[msg.sender] >= fromAmount, "Insufficient balance");
-
-        orders.push(Order({
-            trader: msg.sender,
-            fromToken: fromToken,
-            toToken: toToken,
-            fromAmount: fromAmount,
-            toAmount: toAmount,
-            isBuyOrder: isBuyOrder,
-            isExecuted: false
-        }));
-    }
-
-    function executeTrade(uint256 orderId) external {
-        require(orderId < orders.length, "Invalid order ID");
-        Order storage order = orders[orderId];
-        require(!order.isExecuted, "Order already executed");
-
-        uint256 fee = (order.fromAmount * tradeFee) / 10000; // Calculate the fee
-        uint256 toAmount = order.toAmount - fee;
-
-        if (order.isBuyOrder) {
-            require(balances[msg.sender] >= toAmount, "Insufficient balance");
-            balances[msg.sender] -= toAmount;
-            balances[order.trader] += order.fromAmount;
-        } else {
-            require(balances[msg.sender] >= order.fromAmount, "Insufficient balance");
-            balances[msg.sender] -= order.fromAmount;
-            balances[order.trader] += toAmount;
-        }
-
-        order.isExecuted = true;
-
-        emit TradeExecuted(
-            msg.sender,
-            orderId,
-            order.fromToken,
-            order.toToken,
-            order.fromAmount,
-            toAmount,
-            fee
-        );
-    }
-
-    // Additional functions for managing order books, order matching, and other hybrid exchange features can be added.
-}
-*/
